e1e7df4ce5a10da232910d1bea38de3cf4968adc
Alexander Nalbandov <Alexander.Nalbandov@bx8.me>
2017-03-29 20:59:51 +0300

    Added JS file just

--- ApiNotification/Middleware/ConsoleLoggingComponent.cs
+++ ApiNotification/Middleware/ConsoleLoggingComponent.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 
@@ -10,14 +6,16 @@ namespace ApiNotification.Middleware
 {
     public class ConsoleLoggingComponent : OwinMiddleware
     {
-        public override Task Invoke(IOwinContext context)
-        {
-            Console.WriteLine(string.Format("Requested: " + context.Request.Method + " : " + context.Request.Path));
-            return Next.Invoke(context);
-        }
-
         public ConsoleLoggingComponent(OwinMiddleware next) : base(next)
         {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            Console.WriteLine($"Requested: {context.Request.Method} : {context.Request.Path}");
+            await Next.Invoke(context);
+
+            Console.WriteLine($"Replied {context.Response.StatusCode}");
         }
     }
 }
